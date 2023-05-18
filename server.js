75f8f8f26111c0e5c3f7d90e8fbbfb332f5f4ab6
Hiago Bahú <56275463+hiagovpbahu@users.noreply.github.com>
2023-05-17 21:34:33 -0300

    criação e listagem básica de usuários

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express()
 
 app.use(express.json())
 app.use("/home", express.static('./index.html'))
+app.use("/index.css", express.static('./index.css'))
 app.use("/script.js", express.static('./script.js'))
 
 let users = []
@@ -12,7 +13,7 @@ app.get('/api/user', (request, response) => {
     response.json(users)
 })
 app.post('/api/user', (request, response) => {
-    users.push({
+    const newUser = {
         id: users.length + 1,
         name: request.body.name,
         birthDate: request.body.birthDate,
@@ -20,8 +21,28 @@ app.post('/api/user', (request, response) => {
         cpf: request.body.cpf,
         createdAt: new Date(),
         updatedAt: new Date(),
-    })
+    }
+    users.push(newUser)
+    response.json(newUser)
 })
+
+// serviços => service-types
+//   nome => name
+//   preço => price
+//   duração => duration
+
+// animais => animals
+//   nome => name
+//   raça => breed
+//   idade => age
+//   peso => weight
+//   nome do dono => owner_name
+//   é vacinado => is_vacinated
+
+// atendimentos => services
+//   serviço => service_type
+//   animal => animal
+//   data agendada => scheduled_date
 
 app.listen(3000, () => {
     console.log(`Servidor está rodando em http://localhost:3000`)
